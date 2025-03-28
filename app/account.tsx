0ccc615b8cca876ec760210bd22317bec64cd2b6
Aditya Mathur <aditya.2024@Adityas-MacBook-Pro-7.local>
2025-03-28 15:04:36 +0530

    Bottom Bar created Partially

---
+++ app/account.tsx
@@ -0,0 +1,8 @@
+import { View, Text } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
+
+export default function foryou(){
+    return <SafeAreaView>
+        <Text>Account Page</Text>
+    </SafeAreaView>
+}
